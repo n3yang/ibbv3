5525e4d81b275645be3a20446727d9ea6118b80b
n3yang <n3yang@gmai.com>
2016-03-14 16:05:58 +0800

    add admin module

---
+++ modules/admin/views/default/index.php
@@ -0,0 +1,16 @@
+
+
+<h1 class="page-header">Dashboard</h1>
+
+<div class="admin-default-index">
+    <h1><?= $this->context->action->uniqueId ?></h1>
+    <p>
+        This is the view content for action "<?= $this->context->action->id ?>".
+        The action belongs to the controller "<?= get_class($this->context) ?>"
+        in the "<?= $this->context->module->id ?>" module.
+    </p>
+    <p>
+        You may customize this page by editing the following file:<br>
+        <code><?= __FILE__ ?></code>
+    </p>
+</div>
---
+++ views/layouts/admin.php
@@ -0,0 +1,140 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+
+use app\assets\AdminAsset;
+AdminAsset::register($this);
+
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+            <?
+                if (!Yii::$app->user->isGuest) {
+                    echo '<li>'
+                        . Html::beginForm(['/site/logout'], 'post')
+                        . Html::submitButton(
+                            'Logout (' . Yii::$app->user->identity->username . ')',
+                            ['class' => 'btn btn-link']
+                            )
+                        . Html::endForm()
+                        . '</li>';
+                }
+            ?>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+
+<?
+
+$navItems = [
+        [
+            'label' => 'Dashboard',
+            'url' => Url::toRoute('/admin/'),
+            // 'active' => true,
+        ],
+        [
+            'label' => 'Offer',
+            'url' => Url::toRoute('/admin/offer'),
+        ],
+        [
+            'label' => 'Link',
+            'url' => Url::toRoute('/admin/link'),
+        ],
+        [
+            'label' => 'Tag',
+            'url' => Url::toRoute('/admin/tag'),
+        ],
+        [
+            'label' => 'Offer Tag',
+            'url' => Url::toRoute('/admin/offer-tag'),
+        ],
+        [
+            'label' => 'File',
+            'url' => Url::toRoute('/admin/file'),
+        ],
+        [
+            'label' => 'User',
+            'url' => Url::toRoute('/admin/user'),
+        ],
+        [
+            'label' => 'Login',
+            'url' => ['site/login'],
+        ],
+    ];
+
+preg_match('/(\/admin\/[^\/]+)/', Yii::$app->request->url, $matches) ;
+foreach ($navItems as $key=>$v) {
+  if ($v['url'] == $matches[1]) {
+    $navItems[$key]['active'] = true;
+  }
+}
+
+echo Nav::widget([
+    'items' => $navItems,
+    'options' => ['class' =>'nav nav-sidebar'], // set this to nav-tab to get tab-styled navigation
+]);
+?>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+          </ul>
+
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+          <?= $content ?>
+
+        </div>
+      </div>
+    </div>
+
+
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
