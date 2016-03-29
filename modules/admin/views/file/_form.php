3ff57d669fe126b2103397d1e1e7beff0ca42a48
n3yang <n3yang@gmai.com>
2016-03-29 16:45:56 +0800

    add file views in admin modules

---
+++ modules/admin/views/file/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\File */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="file-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'path')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'mime')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'size')->textInput() ?>
+
+    <?= $form->field($model, 'md5')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'user_id')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ modules/admin/views/file/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\FileSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="file-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'path') ?>
+
+    <?= $form->field($model, 'mime') ?>
+
+    <?= $form->field($model, 'size') ?>
+
+    <?php // echo $form->field($model, 'md5') ?>
+
+    <?php // echo $form->field($model, 'user_id') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
