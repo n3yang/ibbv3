9f2d0a1f77c1d7f6a7c3a9f104821deb097938e3
n3yang <n3yang@gmai.com>
2016-04-23 01:03:51 +0800

    update

--- commands/SpiderController.php
+++ commands/SpiderController.php
@@ -45,11 +45,9 @@ class SpiderController extends Controller
         // $f = $spider->addRemoteFile('http://eimg.smzdm.com/201604/16/5712393eab6aa9184.png');
         // var_dump($f);
 
-        // $spider->syncArticle();
-        $file = File::getUrlById(15);
-        $file = File::getUrlById(15);
-        $file = File::getUrlById(15);
-        var_dump($file);        
+        $spider->syncArticle();
+        // $file = File::getUrlById(15);
+        // var_dump($file);
     }
 
 
