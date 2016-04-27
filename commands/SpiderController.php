a344a38363a53cade20a032bfa6c7d97b9178ab9
n3yang <n3yang@gmai.com>
2016-04-27 19:49:52 +0800

    update

--- commands/SpiderController.php
+++ commands/SpiderController.php
@@ -11,6 +11,7 @@ use yii\helpers\Console;
 use app\models\File;
 use app\models\Offer;
 use app\models\SpiderZdm;
+use app\models\SpiderZdmFx;
 use app\models\Link;
 
 /**
@@ -36,7 +37,19 @@ class SpiderController extends Controller
 
     public function actionSyncZdm($value='')
     {
+
+
+        // $rs = file_get_contents('http://zhushou.huihui.cn/api/hui/latest.json');
+        // $rs = file_get_contents('http://zhushou.huihui.cn/api/myzhushou/collection/list');
+        // $spider = new SpiderZdm;
+        // $rs = $spider->getHttpContent('http://www.mgpyh.com/api/v1/get_post/239730');
+        // print_r(json_decode($rs, 1));
+        // echo date('Y-m-d H:i:s', '1461433041');
+        // echo date('Y-m-d H:i:s', '1461755059');
+        // return;
+
         $spider = new SpiderZdm;
+        $spider->syncArticle();
         // $list = $spider->fetchList('75, 93, 147', 6);
         // var_dump($list);
         // $article = $spider->fetchArticle(6089338);
@@ -46,10 +59,14 @@ class SpiderController extends Controller
         // $f = $spider->addRemoteFile('http://eimg.smzdm.com/201604/16/5712393eab6aa9184.png');
         // var_dump($f);
 
-        $spider->syncArticle();
+        // $spider->syncArticle();
         // $file = File::getUrlById(15);
         // var_dump($file);
         // var_dump(Link::findOneBySlug('gska1cgf'));
+        // $spider->replaceUrl('http://www.smzdm.com/gourl/5D8916EA9AA5AC64/AA_YH_75');
+
+        $spider = new SpiderZdmFx;
+        $spider->syncArticle();
     }
 
 
