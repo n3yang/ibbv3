8aa86b3225277a48f354dec058317dd412b741a7
n3yang <n3yang@gmai.com>
2016-05-24 19:21:41 +0800

    update

--- models/SpiderPyh.php
+++ models/SpiderPyh.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\httpclient\Client;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 // use yii\httpclient\
 use app\models\SpiderBase;
 
@@ -22,6 +23,7 @@ class SpiderPyh extends SpiderBase
     public $dataList = [];
     public $dataArticle = [];
 
+    public $siteUrl = 'http://www.mgpyh.com';
     public $newListApiUrl = 'http://dpapi.mgpyh.com/api/v1/get_more/';
     public $babyListAPiUrl = 'http://dpapi.mgpyh.com/api/v1/category/';
     public $foodListApiUrl = 'http://dpapi.mgpyh.com/api/v1/category/';
@@ -92,14 +94,16 @@ class SpiderPyh extends SpiderBase
             Yii::warning('Fail to fetch list. API return: ' . var_export($rs, 1));
             return [];
         }
-        foreach ($rs['items'] as $r) {
+        foreach ($rs['items'] as &$r) {
             if ($r['is_top']) {
+                unset($r);
                 continue;
             }
-
-            // echo $r['category']."\n";
+            echo $r['category'] .'-'. static::getTagIdByCategoryName($r['category']) ."\n";
+            // echo $this->fetchArticle($r);
+            break;
         }
-        // print_r($rs);
+        return $rs['items'];
     }
 
     public function fetchBabyList()
@@ -112,6 +116,110 @@ class SpiderPyh extends SpiderBase
         return $this->fetchList($this->foodListApiUrl);
     }
 
+
+    public function fetchArticle($a)
+    {
+        // title
+        if (preg_match("/(^.*)\s(.*)$/", $a['post_title'], $matches)) {
+            $title = $matches[1];
+        } else {
+            $title = $a['item_name'];
+        }
+        // price
+        $price = $a['price'];
+        // quick link
+        $linkSlug = $this->replaceUrl($a['money_url'], $title);
+        preg_match("/([\w]+)$/", $linkSlug, $matches);
+        $linkSlug = $matches[1];
+        // excerpt
+        $excerpt = mb_substr(strip_tags($a['post']), 0, 80);
+        // content
+        $content = $this->parseContent($a['post']);
+        // thumbnail
+        $thumbnail = $this->addRemoteFile($a['thumbnail']);
+        $thumb_file_id = $thumbnail['id'];
+        // b2c
+        $b2c = static::getB2cIdByName($a['shop']['name']);
+        // site
+        $fromSite = $this->fromSite;
+
+        $offerDs = [
+            'title'         => $title,
+            'link_slug'     => $linkSlug,
+            'content'       => $content,
+            'price'         => $price,
+            'site'          => $this->fromSite,
+            'b2c'           => $b2c,
+            'status'        => $linkSlug ? Offer::STATUS_DRAFT : Offer::STATUS_PUBLISHED,
+            'excerpt'       => $excerpt,
+            'thumb_file_id' => $thumb_file_id,
+        ];
+
+        $tagId = static::getTagIdByCategoryName($a['category']);
+
+        $this->addOffer($offerDs, [$tagId]);
+
+    }
+
+    public function parseContent($content)
+    {
+        Yii::info('Parsing content...');
+
+        $content = strip_tags($content, '<p><a><br /><span><h2><strong><b><img>');
+
+        $doc = new \DOMDocument();
+        @$doc->loadHTML($content);
+        $tags = $doc->getElementsByTagName('a');
+        for ( $i=0; $i<$tags->length; $i++ ) {
+            $url = $tags[$i]->getAttribute('href');
+            if (empty($url)) {
+                continue;
+            }
+            if (!preg_match('/goods\/\w+/', $url)){
+                $content = str_replace($url, '#', $content);
+            } else {
+                // find the real url
+                $title = utf8_decode($tags->item($i)->nodeValue);
+                $myurl = self::replaceUrl($url, $title);
+                // in content
+                $content = str_replace($url, $myurl, $content);
+            }
+        }
+
+        return $content;
+    }
+
+    public function replaceUrl($url, $title = '')
+    {
+        if (Url::isRelative($url)) {
+            $slash = strpos($url, '/') === 0 ? '' : '/';
+            $url = $this->siteUrl . $slash . $url;
+        }
+        $logstr = 'Replace url: ' . $url;
+
+        if (array_key_exists($url, $this->urlReplaceCache)) {
+            return $this->urlReplaceCache[$url];
+        }
+
+        $real = $this->getRealUrl($url);
+        $logstr.= ' -> ' . $real;
+        // yes, we found the real url, let's replace it to own.
+        if ($real != $url) {
+            // create new short url
+            $link = parent::addLinkUniq($real, $title);
+            $shortUrl = $link['shortUrl'];
+        } else {
+            $shortUrl = '';
+        }
+        // TODO: maybe we can find it in my shortUrl. try it!
+
+        $this->urlReplaceCache[$url] = $shortUrl;
+        
+        Yii::info($logstr . ' -> ' . $shortUrl);
+
+        return $shortUrl;
+    }
+
     public function getRealUrl($url='')
     {
         stream_context_set_default([
@@ -119,8 +227,45 @@ class SpiderPyh extends SpiderBase
                 'header' => 'User-agent: ' . SpiderBase::USER_AGENT
             ]
         ]);
-        $header = get_headers($url, 1);
+        @$header = get_headers($url, 1);
+        if ($header === false) {
+            Yii::warning('Fail to get headers: ' . $url);
+            return parent::getRealUrl($url);
+        }
         $target = is_array($header['Location']) ? $header['Location'][0] : $header['Location'];
         return parent::getRealUrl($target);
+    }
+
+    public function addRemoteFile($src, $name = '')
+    {
+        $src = str_replace('!focus', '', $src);
+        $src = str_replace('!show', '', $src);
+        return parent::addRemoteFile($src, $this->siteUrl, $name);
+    }
+
+    public static function getTagIdByCategoryName($name)
+    {
+        $matches = [
+            20 => ['安全座椅'],
+            18 => ['婴儿推车'],
+            17 => ['LEGO积木拼插', '健身玩具', '益智玩具', '毛绒布艺', '模型玩具', '乐器发声'],
+            14 => ['护肤', '洗护', '清洁'],
+            11 => ['1段', '2段', '3段', '4段'],
+            13 => ['布尿裤', 'M', 'L', 'S'],
+            15 => ['奶瓶奶嘴', '餐具'],
+            12 => ['辅食'],
+        ];
+
+        foreach ($matches as $k => $v) {
+            if (in_array($name, $v)) {
+                return $k;
+            }
+            // else {
+            //     echo 'Fail to get tagId by category name! category name: ' . $name;
+            //     Yii::warning('Fail to get tagId by category name! category name: ' . $name);
+            // }
+        }
+
+        return '';
     }
 }
