96a0ee06e24fc5bb463c87c1010ee3d679f4011a
n3yang <n3yang@gmai.com>
2016-06-06 23:55:21 +0800

    update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,11 +40,11 @@ AppAsset::register($this);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Project name</a>
+                <a class="navbar-brand" href="/">Project name</a>
             </div>
             <div class="navbar-collapse collapse" id="navbar" aria-expanded="false" style="height: 1px;">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li class="active"><a href="/">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
