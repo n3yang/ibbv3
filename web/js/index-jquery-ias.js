dad8ffd2d899361c44fe2bd1067f6b91a7654579
n3yang <n3yang@gmai.com>
2016-07-21 00:07:05 +0800

    add auto load

--- web/js/index-jquery-ias.js
+++ web/js/index-jquery-ias.js
@@ -6,16 +6,8 @@ var ias = jQuery.ias({
   next:       '.next a'
 });
 ias.extension(new IASPagingExtension());
-ias.on('pageChange', function(pageNum, scrollOffset, url) {
-    console.log(
-        "Welcome at page " + pageNum + ", " +
-        "the original url of this page would be: " + url
-    );
-    // $('.pagination .active').addClass('aaa');
-    // console.log(.removeClass('active').next().addClass('active'));
-});
-ias.extension(new IASTriggerExtension({
-    offset: 2,
-    html: $('.pagebar').html(),
-}));
+// ias.extension(new IASTriggerExtension({
+//     offset: 2,
+//     html: $('.pagebar').html(),
+// }));
 ias.extension(new IASSpinnerExtension({}));
